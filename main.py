6c4c5ecf56297febc1b35389e82855c156bd68cd
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 11:17:56 +0300

    solve

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from scapy.all import *
 
+FROM_IFACE = "enp0s8"
+TO_IFACE = "enp0s9"
+
+def send_packet(packet):
+    sendp(packet, iface=TO_IFACE, verbose=False)
+
 def main():
-    pass
+    sniff(iface=FROM_IFACE, prn=send_packet)
 
 
 if __name__ == "__main__":
