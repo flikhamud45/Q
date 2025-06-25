d95c1afe32f54bc764dca3419a6f2c3574592d3a
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 11:58:52 +0300

    Revert "fix"
    This reverts commit 9200397cb7faae20ff7e0bf0ada39562489af5e3.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ port_table: Dict[Tuple[str, int], int] = {}
 port_table_inv: Dict[int, Tuple[str, int]] = {}
 
 # list of firewall rules in the format (protocol, port, allow)
-inbound_firewall_rules: List[Tuple[str, int, bool]] = [("UDP", 12345, False)]
+inbound_firewall_rules: List[Tuple[str, int, bool]] = []
 
 
 def send_packet(packet):
