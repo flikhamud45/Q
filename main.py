c544196a2acf0bb74af42d9f2be6b881e6ae5206
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 15:23:00 +0300

    solve

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def send_packet(packet):
             packet.sport = port_table[(packet[IP].src, packet.sport)]
             packet[Ether].src = TO_IFACE_MAC
             packet[IP].src = TO_IFACE_IP
+            port_table_times[datetime.now()] = packet.sport
             send(packet, iface=TO_IFACE, verbose=True)
     else:
         
@@ -52,6 +53,7 @@ def send_packet(packet):
                     return
 
         if Ether in packet and IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
+            port_table_times[datetime.now()] = packet.dport
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].src = FROM_IFACE_IP
             packet.dport, packet[IP].dst = port_table_inv[packet.dport]
