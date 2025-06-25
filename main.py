753a4ca6938315d1d190693549d31d2d21019502
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 11:59:53 +0300

    solve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 from typing import Dict, Tuple
 import random
+from typing import Dict
 
 FROM_IFACE = "enp0s8"
 TO_IFACE = "enp0s9"
@@ -12,9 +13,12 @@ FROM_IFACE_IP = get_if_addr(FROM_IFACE)
 TO_IFACE_IP = get_if_addr(TO_IFACE)
 
 
-
 port_table: Dict[Tuple[str, int], int] = {}
 port_table_inv: Dict[int, Tuple[str, int]] = {}
+
+# list of firewall rules in the format (protocol, port, allow)
+inbound_firewall_rules: List[Tuple[str, int, bool]] = [("UDP", 12345, False)]
+
 
 def send_packet(packet):
     if packet.sniffed_on == FROM_IFACE:
@@ -31,6 +35,14 @@ def send_packet(packet):
             packet.sport = port_table[(packet[IP].src, packet.sport)]
             send(packet, iface=TO_IFACE, verbose=True)
     else:
+        for protocol, port, allow in inbound_firewall_rules:
+            # the first matching rule will be applied
+            if (protocol == 'TCP' and TCP in packet or protocol == 'UDP' and UDP in packet) and packet.dport == port:
+                if not allow:
+                    return
+                if allow:
+                    break
+            
         if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].src = FROM_IFACE_IP
