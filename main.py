d4b680bedca37b11260fae9eb03ad8d89f9a9bee
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 12:23:02 +0300

    solve

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ def send_packet(packet):
             packet[Ether].src = TO_IFACE_MAC
             packet[IP].src = TO_IFACE_IP
             packet.sport = port_table[(packet[IP].src, packet.sport)]
-            send(packet, iface=TO_IFACE, verbose=False)
+            send(packet, iface=TO_IFACE, verbose=True)
     else:
         if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table:
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].dst = port_table[packet.dport]
-            send(packet, iface=FROM_IFACE, verbose=False)
+            send(packet, iface=FROM_IFACE, verbose=True)
 
 def main():
     sniff(iface=[FROM_IFACE, TO_IFACE], prn=send_packet)
