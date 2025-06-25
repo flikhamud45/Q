368383f1b9b96c34b032ee1226195a15a63e1576
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 15:14:47 +0300

    solve

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TO_IFACE_IP = get_if_addr(TO_IFACE)
 
 port_table: Dict[Tuple[str, int], int] = {}
 port_table_inv: Dict[int, Tuple[str, int]] = {}
-
+port_table_times: Dict[datetime, int] = {}
 
 # list of firewall rules in the format (protocol, port, allow)
 inbound_firewall_rules: Dict[int, Tuple[str, bool]] = {12345: ("UDP", False)}
@@ -25,7 +25,11 @@ def send_packet(packet):
     if packet.sniffed_on == FROM_IFACE:
         if Ether in packet and IP in packet and (TCP in packet or UDP in packet):
             if (packet[IP].src, packet[IP].sport) not in port_table:
-                new_sport = random.choice(list(set(range(1024, 65536)) - set(port_table.values())))
+                avialable_ports = list(set(range(1024, 65536)) - set(port_table.values()))
+                if len(avialable_ports) == 0:
+                    new_sport = port_table_times[min(port_table_times.keys())]
+                else:
+                    new_sport = random.choice(avialable_ports)
                 port_table[(packet[IP].src, packet.sport)] = new_sport
                 port_table_inv[new_sport] = (packet[IP].src, packet.sport)
 
@@ -36,7 +40,7 @@ def send_packet(packet):
     else:
         
             
-        if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
+        if Ether in packet and IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
             if packet.dport in inbound_firewall_rules:
                 protocol, allow = inbound_firewall_rules[packet.dport]
                 if (protocol == "TCP" and TCP in packet) or (protocol == "UDP" and UDP in packet):
