3ccced67b82c43cef34b328aaf982ded8cb25923
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 12:48:31 +0300

    fix

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from scapy.all import *
 FROM_IFACE = "enp0s8"
 TO_IFACE = "enp0s9"
 
+FROM_IFACE_MAC = get_if_hwaddr(FROM_IFACE)
+TO_FACE_MAC = get_if_hwaddr(TO_IFACE)
+
 def send_packet(packet):
     if packet.sniffed_on == FROM_IFACE:
-        sendp(packet, iface=TO_IFACE, verbose=False)
+        if Ether in packet:
+            packet[Ether].src = TO_FACE_MAC
+        send(packet, iface=TO_IFACE, verbose=False)
     else:
+        if Ether in packet:
+            packet[Ether].src = FROM_IFACE_MAC
         sendp(packet, iface=FROM_IFACE, verbose=False)
 
 def main():
