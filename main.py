3c155c59431493bd8cd312de72ef32b7408a57dd
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 15:21:18 +0300

    solve

--- main.py
+++ main.py
@@ -27,14 +27,16 @@ def send_packet(packet):
                     new_sport = random.choice(avialable_ports)
                 port_table[(packet[IP].src, packet.sport)] = new_sport
                 port_table_inv[new_sport] = (packet[IP].src, packet.sport)
-                port_table_times[datetime.now()] = new_sport
+                
 
             packet.sport = port_table[(packet[IP].src, packet.sport)]
             packet[Ether].src = TO_IFACE_MAC
             packet[IP].src = TO_IFACE_IP
+            port_table_times[datetime.now()] = packet.sport
             send(packet, iface=TO_IFACE, verbose=True)
     else:
         if Ether in packet and IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
+            port_table_times[datetime.now()] = packet.dport
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].src = FROM_IFACE_IP
             packet.dport, packet[IP].dst = port_table_inv[packet.dport]
