9dcd619eaa833af91ea6ca07b608ce56dc65188d
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 12:37:35 +0300

    fix

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ TO_IFACE_IP = get_if_addr(TO_IFACE)
 port_table: Dict[Tuple[str, int], int] = {}
 port_table_inv: Dict[int, Tuple[str, int]] = {}
 
-# list of firewall rules in the format (protocol, port, allow)
-inbound_firewall_rules: List[Tuple[str, int, bool]] = []
-
 def send_packet(packet):
     if packet.sniffed_on == FROM_IFACE:
         if Ether in packet and IP in packet and (TCP in packet or UDP in packet):
@@ -34,14 +31,6 @@ def send_packet(packet):
             packet.sport = port_table[(packet[IP].src, packet.sport)]
             send(packet, iface=TO_IFACE, verbose=True)
     else:
-        for protocol, port, allow in inbound_firewall_rules:
-            # the first matching rule will be applied
-            if (protocol == 'TCP' and TCP in packet or protocol == 'UDP' and UDP in packet) and packet.dport == port:
-                if not allow:
-                    return
-                if allow:
-                    break
-            
         if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table.values():
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].src = FROM_IFACE_IP
