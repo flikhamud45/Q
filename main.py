bfd13f370b0efaa95edb09056314e27dd75af46d
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 15:13:47 +0300

    solve

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def send_packet(packet):
             packet[IP].src = TO_IFACE_IP
             send(packet, iface=TO_IFACE, verbose=True)
     else:
-        if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
+        if Ether in packet and IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table_inv:
             packet[Ether].src = FROM_IFACE_MAC
             packet[IP].src = FROM_IFACE_IP
             packet.dport, packet[IP].dst = port_table_inv[packet.dport]
