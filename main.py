78501d7bc0b4ad41928c2a47e16909326aa8896f
Ofek Lavi <flikhamud123@gmail.com>
2025-06-25 12:22:41 +0300

    solve

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ FROM_IFACE = "enp0s8"
 TO_IFACE = "enp0s9"
 
 FROM_IFACE_MAC = get_if_hwaddr(FROM_IFACE)
-TO_FACE_MAC = get_if_hwaddr(TO_IFACE)
+TO_IFACE_MAC = get_if_hwaddr(TO_IFACE)
 
 FROM_IFACE_IP = get_if_addr(FROM_IFACE)
 TO_IFACE_IP = get_if_addr(TO_IFACE)
 
-port_table = {}
+port_table: Dict[Tuple[str, int], int] = {}
 
 def send_packet(packet):
     if packet.sniffed_on == FROM_IFACE:
-        if IP in packet and (TCP in packet or UDP in packet):
+        if Ether in packet and IP in packet and (TCP in packet or UDP in packet):
+            if (packet[IP].src, packet[IP].sport) not in port_table:
+                new_sport = random.randint(1024, 65535)
+                while (packet[IP].src, new_sport) in port_table:
+                    new_sport = random.randint(1024, 65535)
+                port_table[(packet[IP].src, packet.sport)] = new_sport
+            packet[Ether].src = TO_IFACE_MAC
             packet[IP].src = TO_IFACE_IP
-            if not (packet.sport in port_table and port_table[packet.sport] == packet[IP].src):
-                while packet.sport in port_table:
-                    packet.sport = random.randint(1024, 65535)
-                port_table[packet.sport] = packet[IP].src
-        sendp(packet, iface=TO_IFACE, verbose=False)
+            packet.sport = port_table[(packet[IP].src, packet.sport)]
+            send(packet, iface=TO_IFACE, verbose=False)
     else:
         if IP in packet and (TCP in packet or UDP in packet) and packet.dport in port_table:
+            packet[Ether].src = FROM_IFACE_MAC
             packet[IP].dst = port_table[packet.dport]
-        sendp(packet, iface=FROM_IFACE, verbose=False)
+            send(packet, iface=FROM_IFACE, verbose=False)
 
 def main():
     sniff(iface=[FROM_IFACE, TO_IFACE], prn=send_packet)
